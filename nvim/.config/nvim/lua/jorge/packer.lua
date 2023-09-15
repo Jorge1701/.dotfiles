8d115247b1a4cc30555b505bf3a1590626d0f7a8
Jorge Rosas <rosasjorge1701@gmail.com>
2023-09-14 21:05:04 -0300

    Added lsp config

--- nvim/.config/nvim/lua/jorge/packer.lua
+++ nvim/.config/nvim/lua/jorge/packer.lua
@@ -4,24 +4,41 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	-- Telescope
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.2',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    -- Telescope
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-	-- Colorscheme Gruvbox-material
-	use({
-		'sainnhe/gruvbox-material',
-		as = 'gruvbox-material',
-		config = function()
-			vim.cmd('colorscheme gruvbox-material')
-		end
-	})
+    -- Colorscheme Gruvbox-material
+    use({
+        'sainnhe/gruvbox-material',
+        as = 'gruvbox-material',
+        config = function()
+            vim.cmd('colorscheme gruvbox-material')
+        end
+    })
 
-	-- Treesitter
-	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    -- Treesitter
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+
+    -- Lsp-zero
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
+    }
 end)

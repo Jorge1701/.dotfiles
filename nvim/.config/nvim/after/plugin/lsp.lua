3f7d36820597789d07a61344fd9e82d33eb4b47b
Jorge Rosas <rosasjorge1701@gmail.com>
2023-09-16 12:00:40 -0300

    Added shortcuts for lsp

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,7 +1,20 @@
 local lsp = require('lsp-zero').preset({})
 
+local function allow_format(servers)
+    return function(client) return vim.tbl_contains(servers, client.name) end
+end
+
 lsp.on_attach(function(_, bufnr)
-    lsp.default_keymaps({buffer = bufnr})
+    lsp.default_keymaps({ buffer = bufnr })
+    local opts = { buffer = bufnr }
+
+    vim.keymap.set({ 'n', 'x' }, 'gq', function()
+        vim.lsp.buf.format({
+            async = false,
+            timeout_ms = 10000,
+            filter = allow_format({ 'gopls', 'lua_ls' })
+        })
+    end, opts)
 end)
 
 lsp.setup()
@@ -10,6 +23,6 @@ local cmp = require('cmp')
 
 cmp.setup({
     mapping = {
-        ['<CR>'] = cmp.mapping.confirm({select = false}),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
     }
 })
